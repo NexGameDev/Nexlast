a46717e265eff04af536e2086d742c4fb5e6b6a5
NexDev <nexusdevgames@gmail.com>
2026-01-16 09:38:33 +0700

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -1,1 +1,15 @@
-function sleep(ms){ return new Promise(r=>setTimeout(r,ms)); }
+export function deepClone(obj) {
+  return JSON.parse(JSON.stringify(obj));
+}
+
+export function randomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+export function shuffle(arr) {
+  for (let i = arr.length - 1; i > 0; i--) {
+    const j = randomInt(i + 1);
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+  return arr;
+}
