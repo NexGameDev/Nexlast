90bc79ee650f42c0de450765e24fdd1f6d8d0b5c
NexDev <nexusdevgames@gmail.com>
2026-01-16 09:42:20 +0700

    Update game.js

--- js/game.js
+++ js/game.js
@@ -1,12 +1,27 @@
-renderBlocks();
+import { initBoard } from "./board.js";
+import { generateBlocks } from "./blocks.js";
+import { enableDrag } from "./placement.js";
 
-boardEl.onclick=e=>{
-  if(!dragged) return;
-  const x=+e.target.dataset.x;
-  const y=+e.target.dataset.y;
-  if(canPlace(dragged,x,y)){
-    place(dragged,x,y);
-    addScore(dragged.length*10);
-    renderBlocks();
-  }
-};
+const blocksEl = document.getElementById("blocks");
+
+function spawnBlocks() {
+  blocksEl.innerHTML = "";
+  const blocks = generateBlocks();
+
+  const blockObjs = blocks.map(b => {
+    const el = document.createElement("div");
+    el.className = "block";
+    el.style.background = b.color;
+    el.textContent = "";
+    blocksEl.appendChild(el);
+    return { ...b, el };
+  });
+
+  enableDrag(blockObjs, placed => {
+    placed.el.remove();
+    if (!blocksEl.children.length) spawnBlocks();
+  });
+}
+
+initBoard();
+spawnBlocks();
