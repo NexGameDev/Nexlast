dbc821bbd9f4eb9197e6f50f863329943f09119b
NexDev <nexusdevgames@gmail.com>
2026-01-16 09:40:13 +0700

    Update block.js

--- js/block.js
+++ js/block.js
@@ -1,10 +1,30 @@
-const shapes = [
- [[0,0]], [[0,0],[1,0]], [[0,0],[0,1]],
- [[0,0],[1,0],[0,1]],
- [[0,0],[1,0],[2,0]],
- [[0,0],[0,1],[0,2]]
+import { shuffle } from "./utils.js";
+
+export const SHAPES = [
+  [[0,0]],
+  [[0,0],[1,0]],
+  [[0,0],[0,1]],
+  [[0,0],[1,0],[0,1]],
+  [[0,0],[1,0],[2,0]],
+  [[0,0],[0,1],[0,2]],
+  [[0,0],[1,0],[0,1],[1,1]],
+  [[0,0],[1,0],[2,0],[3,0]],
+  [[0,0],[0,1],[0,2],[0,3]],
 ];
 
-function randomBlock(){
-  return shapes[Math.floor(Math.random()*shapes.length)];
+const COLORS = [
+  "#ff5f5f",
+  "#ffb703",
+  "#8ecae6",
+  "#9b5de5",
+  "#00f5d4",
+  "#f15bb5"
+];
+
+export function generateBlocks() {
+  const blocks = shuffle([...SHAPES]).slice(0, 3);
+  return blocks.map(shape => ({
+    shape,
+    color: COLORS[Math.floor(Math.random() * COLORS.length)]
+  }));
 }
